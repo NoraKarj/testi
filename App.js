308fd18458982d69a8a0644a21fcd04469c29328
NoraKarj <t1kano00@students.oamk.fi>
2023-09-01 15:03:59 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [euros, setEuros] = useState('')
+  const [krona, setKrona] = useState(0)
+
+const calculate = () => {
+    const result = euros.replace(',','.') * 11.85 //Numero on kruunujen arvo ja pilkku vaihdetaan pisteeksi
+    setKrona(result)
+}
+
+/*function calculate(){
+Tämä vanha tapa kirjoittaa yläpuolella uusi
+} */
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Euros</Text>
+      <TextInput placeholder='Enter amount of euros'
+      keyboardType='decimal-pad'
+      value={euros}
+      onChangeText={text => setEuros(text)} //Tämän avulla voit kirjottaa euro kohtaan
+      style={styles.field}
+      />
+      <Text style={styles.field}>Krona</Text>
+      <Text style={styles.field}>{krona.toFixed(2)}</Text>  
+      <Button title='Calculate' onPress={calculate}/>
     </View>
   );
+  //Saadaan toFixed komennolla näkymään 2 desimaalin tarkkuudella kruunut
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    marginLeft: 10,
+    marginRight:10,
+
   },
+  field: {
+    marginBottom:10,
+
+  }
 });
